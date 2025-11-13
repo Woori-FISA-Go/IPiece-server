1fb6136c01c3d2a2eb2e78303496b2f9e7c845c1
kkangsol <kangsol0924@gmail.com>
2025-11-13 09:41:15 +0900

    [#14] 공통 예외처리 로직 통합
    핸들러, 커스텀 예외 클래스 생성 및 에러 코드 추가

---
+++ src/main/java/com/masterpiece/IPiece/common/exception/BusinessException.java
@@ -0,0 +1,15 @@
+package com.masterpiece.IPiece.common.exception;
+
+import lombok.Getter;
+
+// 우리가 직접 생성한 예외 클래스(이걸로 호출할 예정)
+
+@Getter
+public class BusinessException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
--- src/main/java/com/masterpiece/IPiece/common/exception/ErrorCode.java
+++ src/main/java/com/masterpiece/IPiece/common/exception/ErrorCode.java
@@ -3,17 +3,30 @@ package com.masterpiece.IPiece.common.exception;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+//필요한 에러코드와 메세지 추가하며 사용 예정
+
 @Getter
 public enum ErrorCode {
 
     /* 공통 - 4xx */
     VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "입력값이 유효하지 않습니다."),
-    AUTH_REQUIRED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
+    AUTH_REQUIRED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),   //미로그인
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),   //위조,형식 오류
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),   //토큰만료
     PERMISSION_DENIED(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 리소스를 찾을 수 없습니다."),
     CONFLICT(HttpStatus.CONFLICT, "요청이 충돌했습니다."),
     UNPROCESSABLE_ENTITY(HttpStatus.UNPROCESSABLE_ENTITY, "도메인 규칙 위반입니다."),
     RATE_LIMITED(HttpStatus.TOO_MANY_REQUESTS, "요청이 너무 많습니다."),
+
+    /* 인증/로그인 */
+    INVALID_LOGIN_ID(HttpStatus.BAD_REQUEST, "존재하지 않는 아이디입니다."),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    SMS_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "인증번호 발송 실패"),
+    INVALID_VERIFICATION_CODE(HttpStatus.BAD_REQUEST, "인증번호가 일치하지 않습니다."),
+    EXPIRED_VERIFICATION_CODE(HttpStatus.BAD_REQUEST, "인증번호가 만료되었습니다."),
+
+
     /* 공통 - 5xx */
     INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다."),
     SERVICE_UNAVAILABLE(HttpStatus.SERVICE_UNAVAILABLE, "현재 서비스 이용이 불가합니다."),
