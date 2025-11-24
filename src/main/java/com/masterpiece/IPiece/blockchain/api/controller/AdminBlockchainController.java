a202d0b73be587b0bf86aca70be00ff46c9d28c4
Gill010147 <qudrlftm0104@gmail.com>
2025-11-24 17:08:27 +0900

    [#126] fix: 블록체인 api swagger 검증테스트 이슈에 대한 수정
    블록체인 api swagger 검증테스트 이슈에 대한 수정

--- src/main/java/com/masterpiece/IPiece/blockchain/api/controller/AdminBlockchainController.java
+++ src/main/java/com/masterpiece/IPiece/blockchain/api/controller/AdminBlockchainController.java
@@ -6,6 +6,7 @@ import com.masterpiece.IPiece.blockchain.application.BlockchainService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -28,6 +29,7 @@ public class AdminBlockchainController {
     @PreAuthorize("hasRole('ADMIN')")
     @Operation(summary = "블록체인 토큰 목록 조회", security = @SecurityRequirement(name = "JWT"))
     public ResponseEntity<TokenListResponse> getTokenList(
+            @ParameterObject
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
         TokenListResponse response = blockchainService.getTokenList(pageable);
         return ResponseEntity.ok(response);
